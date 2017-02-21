dfc61ef1dfd762ca6c1802d9b51ba4421111e104
Le-Khoi Phong <phong.lekhoi@gmail.com>
2017-02-21 12:26:32 +0800

    clean trackers intervally

--- background/index.js
+++ background/index.js
@@ -1,8 +1,10 @@
 import QuoteBackground from './quote';
 import followThread from './followThread';
+import startCleanTracker from './followThread/cleanTracker';
 
 const quoteBackground = new QuoteBackground();
 followThread();
+startCleanTracker();
 
 export {
   quoteBackground,
