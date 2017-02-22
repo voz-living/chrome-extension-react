199ec9035b324e365c0c9634d0f2b1137ddf090f
Le-Khoi Phong <phong.lekhoi@gmail.com>
2017-02-22 19:45:45 +0800

    Revert "add migration from local to sync"
    This reverts commit 24a1c5968f55122846ddc53973a446bfb4696180.

--- background/index.js
+++ background/index.js
@@ -6,17 +6,6 @@ const quoteBackground = new QuoteBackground();
 followThread();
 startCleanTracker();
 
-chrome.storage.sync.get('_migrated', (result) => {
-  if (!result._migrated) {
-    chrome.storage.local.get(null, (oldStorage) => {
-      chrome.storage.sync.set({ ...oldStorage, _migrated: true }, () => {
-        console.info('Migration done');
-      });
-      chrome.storage.local.set({ '_migrated_local': true });
-    });
-  }
-});
-
 export {
   quoteBackground,
 };
