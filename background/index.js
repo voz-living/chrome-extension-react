78198869e9d8ef45adc8cd8dd43ded2f2edf28bd
Le-Khoi Phong <phong.lekhoi@gmail.com>
2017-02-23 03:48:08 +0800

    add feedback form when uninstall

--- background/index.js
+++ background/index.js
@@ -5,7 +5,7 @@ import startCleanTracker from './followThread/cleanTracker';
 const quoteBackground = window.quoteBackground = new QuoteBackground();
 followThread();
 startCleanTracker();
-
+chrome.runtime.setUninstallURL('https://goo.gl/forms/hA9IzC8XRvJH7pxj2'); // eslint-disable-line no-undef
 export {
   quoteBackground,
 };
