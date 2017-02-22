9a09329ad2430d503637df5a4b3c8d9638ed5e14
Le-Khoi Phong <phong.lekhoi@gmail.com>
2017-02-22 19:46:00 +0800

    Revert "switch to use sync storage"
    This reverts commit 339f430ee9c7f3234a76a529737396f0f64ad322.

--- app/utils/settings.js
+++ app/utils/settings.js
@@ -33,7 +33,7 @@ const defaultSettingKeys = _.keys(defaultStoreStructure);
 /* eslint-disable no-undef */
 export const getChromeLocalStore = (keys = defaultSettingKeys) => (
   new Promise(resolve => {
-    chrome.storage.sync.get(keys, items => {
+    chrome.storage.local.get(keys, items => {
       if (_.isEmpty(items)) {
         const result = { };
         let outKeys = keys;
@@ -59,7 +59,7 @@ export const getChromeLocalStore = (keys = defaultSettingKeys) => (
 
 export const setChromeLocalStore = items => (
   new Promise(resolve => {
-    chrome.storage.sync.set(items, () => resolve(true));
+    chrome.storage.local.set(items, () => resolve(true));
   })
 );
 /* eslint-enable no-undef */
