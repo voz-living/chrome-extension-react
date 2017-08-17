3e723c39c07d8e053cd5ad89772ad06ed77c0be9
Le-Khoi Phong <phong.lekhoi@gmail.com>
2017-08-17 19:27:26 +0800

    Caching the request to localstorage

--- app/components/StickerPicker.js
+++ app/components/StickerPicker.js
@@ -27,6 +27,34 @@ export default class StickerPicker extends Component {
   }
 
   prepareStickerData() {
+    const storageData = window.localStorage.getItem('vozLivingStickerData');
+    if (storageData === null) {
+      return this.downloadStickerData()
+        .then(data => {
+          window.localStorage.setItem('vozLivingStickerData', JSON.stringify(data));
+          window.localStorage.setItem('vozLivingStickerData_lastUpdate', new Date().getTime());
+          return data;
+        }).catch((e) => {
+          console.log(e);
+        });
+    }
+    const data = JSON.parse(storageData);
+    setTimeout(() => {
+      const lastUpdate = window.localStorage.getItem('vozLivingStickerData_lastUpdate');
+      if (lastUpdate === null || (new Date().getTime() - parseInt(lastUpdate, 10) > 1000 * 60 * 60)) {
+        this.downloadStickerData()
+          .then(dlData => {
+            const str = JSON.stringify(dlData);
+            if (str === storageData) return;
+            window.localStorage.setItem('vozLivingStickerData', str);
+            window.localStorage.setItem('vozLivingStickerData_lastUpdate', new Date().getTime());
+          }).catch(() => {});
+      }
+    }, 2000);
+    return Promise.resolve(data);
+  }
+
+  downloadStickerData() {
     const stickerManifestUrl = stickerUrl + '/declare.json';
     return PROXY_GET(stickerManifestUrl, { credentials: 'no-cors' })
       .then(res => {
