3c95542032f6acc62f6452f6c8edc35d3d9b8d98
SuperDOgePx <30575847+SuperDOgePx@users.noreply.github.com>
2017-10-26 00:29:14 +0700

    chỉnh sửa

--- app/components/EyesProtect/index.js
+++ app/components/EyesProtect/index.js
@@ -1,38 +1,68 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 import $ from 'jquery';
-
+import { autobind } from 'core-decorators';
+@autobind
 class EyesProtect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { timer: 30 };
+  }
   componentWillMount() {
     this.darkMode();
+    const wrapper = document.createElement('div');
+    wrapper.id = 'voz-warm-wrapper';
+    document.body.appendChild(wrapper);
+    render(this.warmMode(), wrapper);
+    const eyesWrap = document.createElement('div');
+    wrapper.appendChild(eyesWrap);
+    render(this.eyeNotify(), eyesWrap);
   }
   darkMode() {
     require('../../styles/dark-mode.less');
   }
-  fluxMode() {
+  warmMode() {
+    document.documentElement.style.backgroundColor = '#111111';
     return (
       <div
         className="voz-warm-filter"
-        style={`
-    background-color: rgba(218, 84, 6, 0.5);
-    position: fixed;
-    width: 120%;
-    height: 120%;
-    mix-blend-mode: multiply;
-    z-index: 9999999;
-    pointer-events: none;`}
+        style={{
+          backgroundColor: 'rgba(218, 84, 6, 0.5)',
+          position: 'fixed',
+          width: '120%',
+          height: '120%',
+          top: '0',
+          left: '0',
+          mixBlendMode: 'multiply',
+          zIndex: '9999999',
+          pointerEvents: 'none' }}
       />
       );
   }
+  countDown() {
+    let { timer } = this.state;
+    setInterval(() => {
+      if (timer > 0) {
+        this.setState({ timer });
+        timer -= 1;
+      }
+      console.log(timer);
+    }, 1000);
+    return { timer };
+}
   eyeNotify() {
+    this.countDown();
     return (
       <div className="voz-bao-ve-mat">
-        <div style="position:fixed;height: 100%;width: 100%;top: 0;z-index: 99;background: rgba(0, 0, 0, 0.8);font-size:15px" />
-          <div style="text-align:center;padding: 50px;background-color: #4286f4;color: white;  position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index:99;font-size:15px">
-              <strong> Vì một đôi mắt khoẻ mạnh, các thím hãy cùng nhau không nhìn vào máy tính trong lúc này nào! </strong>
-              <img style="vertical-align:middle" src="https://vozforums.com/images/smilies/Off/byebye.gif" /><br />
-              <strong> Thông báo này sẽ tự tắt sau </strong><strong className="dem-nguoc"></strong>
-              <div className="eyes-close" style="position:absolute;width:150px;height:50px;background-color:green;right:0;bottom:0;cursor:pointer; display:flex;align-items: center; justify-content:center;">Tắt thông báo</div>
-          </div>
+        <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0, zIndex: 9999, background: 'rgba(0, 0, 0, 0.8)', fontSize: '15px' }} />
+        <div style={{ textAlign: 'center', padding: '50px', backgroundColor: '#4286f4', color: 'white', position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 10000, fontSize: '15px' }}>
+          <strong> Vì một đôi mắt khoẻ mạnh, các thím hãy cùng nhau không nhìn vào máy tính trong lúc này nào! </strong>
+          <img style={{ verticalAlign: 'middle' }} src="https://vozforums.com/images/smilies/Off/byebye.gif" /><br />
+          <strong> Thông báo này sẽ tự tắt sau </strong>
+          <strong className="dem-nguoc">{this.state.timer} giây</strong>
+          <div className="eyes-close" onClick={() => $('.voz-bao-ve-mat').remove()} style={{ position: 'absolute', width: '150px', height: '50px', backgroundColor: 'green', right: 0, bottom: 0, cursor: 'pointer', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Tắt thông báo</div>
+        </div>
       </div>
             );
   }
